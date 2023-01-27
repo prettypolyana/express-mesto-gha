c2555c784a001d039d7ee255df270b2ab34b5903
Polina Kulik <pollyandrrrr@yandex.ru>
2023-01-27 21:13:57 +0300

    fix: внести правки по тестам 2

--- routes/cards.js
+++ routes/cards.js
@@ -12,8 +12,8 @@ const {
 router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    link: Joi.string().regex(/https?:\/\/(www\.)?[\d\S]+$/i),
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().regex(/https?:\/\/(www\.)?[\d\S]+$/i),
   }),
 }), createCard);
 router.delete('/:cardId', celebrate({
